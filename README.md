a5b034e2b39ed27cc7524fb5d1c5efcccebd9502
Gus Ridgway <auridgway@gmail.com>
2024-01-08 18:47:21 -0600

    framed out approach and declared some variables with functions

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ It is you versus the Freaky Fungus, an evil mycological menace from ***Duluth***
 
 When you click an attack button:
 
--  update _state_ variable(s) to make the Freaky Fungus lose hit points (HP), and to reduce your attack points (AP). 
+- update _state_ variable(s) to make the Freaky Fungus lose hit points (HP), and to reduce your attack points (AP). 
 - See [Attacks](#attacks) below, for the AP and HP values of each attack
 - State may be held in one our more variables of your choosing
 - HP and AP values may not be negative (set to zero, if they would otherwise be negative)
--- script.js
+++ script.js
@@ -1,19 +1,58 @@
 // State Variables can be declared outside of the onReady
 // Feel free to make this to what you want!
 // Example:
-// let fungusHP = 100;
+const spells = [{
+    name: 'arcane-scepter',
+    Cost: 12,
+    Damage: 14
+},
+{
+    name: 'entangle',
+    Cost: 23,
+    Damage: 9
+},
+{
+    name: 'dragon blade',
+    Cost: 38,
+    Damage: 47
+},
+{
+    name: 'star-fire',
+    Cost: 33,
+    Damage: 25
+}];
+
+let fungusHP = 100;
+let yourAP = 100;
 
 function onReady() {
     console.log("Ready to go!")
-    
+
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
-    
-    
+
+
     // 🧠 Remember
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
+}
+
+function spellCast(spellName) {
+    // update variables from attack - they cant fall below zero
+
+
+}
+
+function renderDom() {
+    // update hp and ap
+
+    // if his hp = 0 then you win and replace walk to dead class
+
+    // if your ap = 0 then you lose and replace walk to jump class
+
+    // if ap = 0 then disable all abilities
+
 }
 
 
